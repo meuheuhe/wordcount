3f255d6075d310c3ef5c7fd6a6e77a7a429ba49a
kimkijung <rlarlwnd61@gmail.com>
2019-01-21 17:31:56 +0900

    first

--- firstproject/settings.py
+++ firstproject/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wordCount.apps.WordcountConfig',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,7 +58,7 @@ ROOT_URLCONF = 'firstproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- wordCount/views.py
+++ wordCount/views.py
@@ -1,23 +1,35 @@
+import re
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {'hbar':'home'})
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html', {'abar':'about'})
+def kor(request):
+    return render(request, 'kor.html', {'kbar':'kor'})
+def eng(request):
+    return render(request, 'eng.html', {'ebar':'eng'})
+
 def result(request):
-    text = request.GET['fulltext']
-    words = text.split()
-    word_dictionary = {}
+            text = request.GET['fulltext']
+            hangul = re.compile('[^ \u3131-\u3163\uac00-\ud7a3]+')
+            result = hangul.sub('',text)
+            anoder_result = hangul.findall(text)
+            
+            d = re.compile('[^0-9]')
 
-    for word in words:
-        if word in word_dictionary:
-            #increase
-            word_dictionary[word] += 1
-        else:
-            # add to dictionary
-            word_dictionary[word] = 1
+            r = d.sub('',text) 
 
 
-    return render(request, 'result.html', {'full':text, 'total': len(words), 'dictionary': word_dictionary.items()})
+
+            return render(request, 'result.html', {'full':text , 're':result, 'a':r})
+
+def result_han(request):
+    
+    return render(request, 'request_han.html', {'gbar':'result_han'})
+
+
+def result_eng(request):
+    return render(request, 'request_eng.html', {'ebar':'result_eng'})
